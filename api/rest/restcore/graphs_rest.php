b8b36df80d752fbc447b081884be54d36fc0111d
Muhammad Afaq <afaq.centosquare@gmail.com>
2024-09-19 20:49:05 +0500

    debug query result

--- api/rest/restcore/graphs_rest.php
+++ api/rest/restcore/graphs_rest.php
@@ -26,7 +26,7 @@ function rest_dashboard_get(Request $p_request, Response $p_response, array $p_a
 		$t_project_id = helper_get_current_project();
 		$t_user_id = auth_get_current_user_id();
 		$t_specific_where = helper_project_specific_where($t_project_id, $t_user_id);
-		$t_resolved_status_threshold = config_get('bug_resolved_status_threshold');
+ 	$t_resolved_status_threshold = config_get('bug_resolved_status_threshold');
 
 		$t_query = new DBQuery();
 		$t_sql = 'SELECT handler_id, count(*) as count FROM {bug} WHERE ' . $t_specific_where
@@ -38,34 +38,34 @@ function rest_dashboard_get(Request $p_request, Response $p_response, array $p_a
 		}
 		$t_sql .= ' GROUP BY handler_id ORDER BY count DESC';
 		$t_query->sql($t_sql);
-		$t_query->bind(array(
-				'nouser' => NO_USER,
-				'status_resolved' => (int)$t_resolved_status_threshold,
-		));
-//
-//		$t_handler_array = array();
-//		$t_handler_ids = array();
-//
-//		while ($t_row = $t_query->fetch()) {
-//				$t_handler_array[$t_row['handler_id']] = (int)$t_row['count'];
-//				$t_handler_ids[] = $t_row['handler_id'];
-//		}
-//
-//		if (count($t_handler_array) == 0) {
-//				return $p_response->withStatus(HTTP_STATUS_SUCCESS)->withJson([
-//						'data' => []
-//				]);
-//		}
-//
-//		user_cache_array_rows($t_handler_ids);
-//
-//		foreach ($t_handler_array as $t_handler_id => $t_count) {
-//				$t_metrics[user_get_name($t_handler_id)] = $t_count;
-//		}
-//
-//		arsort($t_metrics);
+//		$t_query->bind(array(
+//				'nouser' => NO_USER,
+//				'status_resolved' => (int)$t_resolved_status_threshold,
+//		));
+
+		$t_handler_array = array();
+		$t_handler_ids = array();
+
+		while ($t_row = $t_query->fetch()) {
+				$t_handler_array[$t_row['handler_id']] = (int)$t_row['count'];
+				$t_handler_ids[] = $t_row['handler_id'];
+		}
+
+		if (count($t_handler_array) == 0) {
+				return $p_response->withStatus(HTTP_STATUS_SUCCESS)->withJson([
+						'data' => []
+				]);
+		}
+
+		user_cache_array_rows($t_handler_ids);
+
+		foreach ($t_handler_array as $t_handler_id => $t_count) {
+				$t_metrics[user_get_name($t_handler_id)] = $t_count;
+		}
+
+		arsort($t_metrics);
 		
 		return $p_response->withStatus(HTTP_STATUS_SUCCESS)->withJson([
-				'data' => $t_query->fetch()
+				'data' => $t_metrics
 		]);
 }
